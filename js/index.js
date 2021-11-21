b4310557a82186b98cc7230428ce9bdf38f64301
Александр <westerovs@mail.ru>
2021-11-22 02:02:57 +0300

    render color

--- js/index.js
+++ js/index.js
@@ -1,22 +1,55 @@
-const color = document.querySelector('.color')
 const modal = document.querySelector('.modal')
-const box = color.querySelector('.color__box')
+const box = document.querySelector('.color-box')
 
 const fieldR = modal.querySelector('.modal__label-R')
 const fieldG = modal.querySelector('.modal__label-G')
 const fieldB = modal.querySelector('.modal__label-B')
 
-const getValueColor = () => {
+const rgbArray = [
+  {
+    field: {
+      r: 1,
+      g: 2,
+      b: 3,
+    }
+  }, {
+    field: {
+      r: 10,
+      g: 10,
+      b: 10,
+    }
+  }, {
+    field: {
+      r: 20,
+      g: 20,
+      b: 20,
+    }
+  }, {
+    field: {
+      r: 30,
+      g: 30,
+      b: 30,
+    }
+  }
+]
+
+const getValueColor = (event) => {
+  console.log(event.target)
+  
   const colorValueR = document.querySelector('#modal__input-R').value
   const colorValueG = document.querySelector('#modal__input-G').value
   const colorValueB = document.querySelector('#modal__input-B').value
 }
 
-const setColor = (R, G, B) => {
-  box.style.backgroundImage = `
-    linear-gradient(to right bottom, rgb(${ R }, ${ G }, ${ B }), transparent 100%),
-    linear-gradient(to left bottom, rgb(${ 0 }, ${ 0 }, ${ 0 }), transparent 100%),
-    linear-gradient(to left top, rgb(${ 0 }, ${ 0 }, ${ 0 }), transparent 50%),
-    linear-gradient(to right top, rgb(${ 0 }, ${ 0 }, ${ 0 }), transparent 100%)
-  `
+const renderColor = () => {
+  rgbArray.find(color => {
+    box.style.backgroundImage = `
+    linear-gradient(to right bottom, rgb(${ color.field.r }, ${ color.field.g }, ${ color.field.b }), transparent 100%),
+    linear-gradient(to left bottom, rgb(${ color.field.r }, ${ color.field.g }, ${ color.field.b }), transparent 100%),
+    linear-gradient(to left top, rgb(${ color.field.r }, ${ color.field.g }, ${ color.field.b }), transparent 50%),
+    linear-gradient(to right top, rgb(${ color.field.r }, ${ color.field.g }, ${ color.field.b }), transparent 100%)`
+  })
 }
+
+
+modal.addEventListener('change', getValueColor)
